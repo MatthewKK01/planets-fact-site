b30fbeba69c43fa504e6e0ee36967ab2d5084d61
Mate Kavelashvili <matthewkk@Mates-MacBook-Air.local>
2023-09-05 22:31:59 +0400

    fixed dynamic routes.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Planet } from 'src/models/Planets';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  @Input() public HeaderPlanets: any;
+  constructor(private router: Router) { }
+
+  navigate(url: any) {
+    this.router.navigate(['planets', url]);
+  }
 
 }
--- src/app/planet/planet.component.ts
+++ src/app/planet/planet.component.ts
@@ -1,15 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
-
+import { ActivatedRoute } from '@angular/router';
+import { Planet } from 'src/models/Planets';
 
 @Component({
   selector: 'app-planet',
   templateUrl: './planet.component.html',
   styleUrls: ['./planet.component.css']
 })
-export class PlanetComponent {
+export class PlanetComponent implements OnInit {
+  name: string = "";
+  planets: Planet[] | any;
+  selectedPlanet: Planet[] | any;
 
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getPlanet();
+    this.route.params.subscribe(params => {
+      this.name = params['name'];
+      console.log(this.planets);
+    });
+  }
+
+  getPlanet() {
+    this.http.get<Planet[]>("assets/data/data.json").subscribe((planets) => {
+      this.planets = planets;
+      let selectedPlanet = this.planets.filter((planet: any) => planet.name === this.name);
+      console.log(selectedPlanet);
+    });
+  }
 
 
 }
