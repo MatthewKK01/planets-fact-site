83448d11ece18699ee98361e9133bd880d2142f0
Mate Kavelashvili <matthewkk@Mates-MacBook-Air.local>
2023-09-07 20:07:26 +0400

    created mobile menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,10 +10,44 @@ import { Planet } from 'src/models/Planets';
 })
 export class HeaderComponent {
   @Input() public HeaderPlanets: Planet[] | undefined;
-  constructor(private router: Router) { }
+  active: boolean = false;
+  constructor(private router: Router, private route: ActivatedRoute) { }
+  name: string = "";
+
+  ngOnInit() {
+    this.name = this.route.snapshot.params['name'];
+  }
+
+  getPlanetClass(planetName: string): string {
+    switch (planetName) {
+      case 'mercury':
+        return 'bg-[#419EBB]';
+      case 'venus':
+        return 'bg-[#EDA249]';
+      case 'earth':
+        return 'bg-[#6D2ED5]';
+      case 'mars':
+        return 'bg-[#D14C32]';
+      case 'jupiter':
+        return 'bg-[#D83A34]';
+      case 'saturn':
+        return 'bg-[#CD5120]';
+      case 'uranus':
+        return 'bg-[#1EC1A2]';
+      case 'neptune':
+        return 'bg-[#2D68F0]';
+      default:
+        return 'bg-none';
+    }
+  }
+
 
   navigate(url: string) {
     this.router.navigate(['planets', url.toLowerCase()]);
+    this.active = false;
+  }
+  openMenu() {
+    this.active = !this.active
   }
 
 }
--- src/app/planet/planet.component.ts
+++ src/app/planet/planet.component.ts
@@ -46,6 +46,29 @@ export class PlanetComponent implements OnInit {
         return 'bg-none';
     }
   }
+  underLine(): string {
+    switch (this.name) {
+      case 'mercury':
+        return ' border-b-4 border-[#419EBB]';
+      case 'venus':
+        return 'border-b-4 border-[#EDA249]';
+
+      case 'earth':
+        return 'border-b-4 border-[#6D2ED5]';
+      case 'mars':
+        return 'border-b-4 border-[#D14C32]';
+      case 'jupiter':
+        return 'border-b-4 border-[#D83A34]';
+      case 'saturn':
+        return 'border-b-4 border-[#CD5120]';
+      case 'uranus':
+        return 'border-b-4 border-[#1EC1A2]';
+      case 'neptune':
+        return 'border-b-4 border-[#2D68F0]';
+      default:
+        return 'bg-none';
+    }
+  }
 
   switchCategory(e: any) {
     let value = e.target.value;
